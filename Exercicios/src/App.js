83974f6ebeae0c9e3525fad26faa98d5a202535b
Rudá Torquato <rudatorquato@gmail.com>
2021-02-01 13:16:45 -0300

    Organizando componentes

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
-export default () => <Text>Primeiro Componente!!!</Text>
+import Primeiro from './components/Primeiro'
+
+export default () => (
+    <View>
+        <Primeiro />
+    </View>
+)
