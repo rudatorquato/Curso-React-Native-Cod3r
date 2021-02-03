e09ae6c0cd2239721fc5538c7b5a93322a5ca6ef
Rudá Torquato <rudatorquato@gmail.com>
2021-02-03 15:07:17 -0300

    Componente com propriedades

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import CompPadrao ,{ Comp1, Comp2 } from './components/Multi'
-import Primeiro from './components/Primeiro'
+import MinMax from './components/MinMax'
+//import CompPadrao ,{ Comp1, Comp2 } from './components/Multi'
+//import Primeiro from './components/Primeiro'
 
 export default () => (
     <View style={style.App}>
+        <MinMax min="3" max="20"/>
+        <MinMax min={3} max={20}/>
+        {/*
         <CompPadrao />
         <Comp1 />
         <Comp2 />
-
         <Primeiro />
+        */}
     </View>
 )
 
 const style = StyleSheet.create({
     App: {
-        flexGrow:1,
-        justifyContent:"center",
-        alignItems: "center"
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20
     }
 })
--- Exercicios/src/components/Multi.js
+++ Exercicios/src/components/Multi.js
@@ -3,15 +3,15 @@ import { Text } from 'react-native'
 import Estilo from './estilo'
 
 function Comp() {
-    return <Text style={Estilo.fontG}>Comp #Oficial</Text>
+    return <Text style={Estilo.txtG}>Comp #Oficial</Text>
 }
 
 function Comp1() {
-    return <Text style={Estilo.fontG}>Comp #1</Text>
+    return <Text style={Estilo.txtG}>Comp #1</Text>
 }
 
 function Comp2() {
-    return <Text style={Estilo.fontG}>Comp #2</Text>
+    return <Text style={Estilo.txtG}>Comp #2</Text>
 }
 
 export {Comp1, Comp2}
