14c128ee97a202364b0512a9a4a420e0c1cc7a13
Rudá Torquato <rudatorquato@gmail.com>
2021-02-03 17:51:38 -0300

    numeros aleatorios

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-import MinMax from './components/MinMax'
+import Aleatorio from './components/Aleatorio'
+//import MinMax from './components/MinMax'
 //import CompPadrao ,{ Comp1, Comp2 } from './components/Multi'
 //import Primeiro from './components/Primeiro'
 
 export default () => (
     <View style={style.App}>
+        <Aleatorio min={1} max={50}/>
+        {/*
         <MinMax min="3" max="20"/>
         <MinMax min={3} max={20}/>
-        {/*
+       
         <CompPadrao />
         <Comp1 />
         <Comp2 />
