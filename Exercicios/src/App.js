a9a05f0a5f182854173017ec547184caae9b4158
Rudá Torquato <rudatorquato@gmail.com>
2021-02-15 16:01:46 -0300

    Renderizando Lista #02

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {  StyleSheet, SafeAreaView } from 'react-native'
 
-import ListaProdutos from './components/produtos/ListaProdutos'
+import ListaProdutosV2 from './components/produtos/ListaProdutosV2'
+//import ListaProdutos from './components/produtos/ListaProdutos'
 //import UsuarioLogado from  './components/UsuarioLogado'
 //import Familia from './components/relacao/Familia'
 //import Membro from './components/relacao/MembroFamilia'
@@ -20,8 +21,10 @@ import ListaProdutos from './components/produtos/ListaProdutos'
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <ListaProdutos />
+        
+        <ListaProdutosV2 />
         {/*
+        <ListaProdutos />
         <UsuarioLogado usuario={{ nome: 'Torquato', email: 'teste@teste.com' }} />
         <UsuarioLogado usuario={{ nome: 'Ana' }} />
         <UsuarioLogado usuario={{ email: 'carlos@empresa.com' }} />
